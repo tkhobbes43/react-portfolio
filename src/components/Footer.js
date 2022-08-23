4fd7b93fb41406a358e00c7dda89d443f819d79f
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-23 13:09:48 -0500

    installed react-icons, got footer to render!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,11 @@
 import React, { useState } from 'react';
+import {
+    FiLinkedin,
+    FiMail,
+    FiGithub,
+    FiFile
+} from 'react-icons/fi';
+
 
 function Footer() {
     return (
@@ -10,20 +17,31 @@ function Footer() {
                 <h3>Copyright © 2022</h3>
             </div>
             <div className='footer-body'>
-                <ul className='footer-icons'>
-                    <li className='social-icons'>
-                        <a href="mailto: t.k.hobbes43@gmail.com">Email Me</a>
-                    </li>
-                    <li className='social-icons'>
-                        <a href="https://github.com/tkhobbes43" target="_blank" rel="noreferrer noopener">My GitHub</a>
-                    </li>
-                    <li className='social-icons'>
-                        <a href="https://www.linkedin.com/in/thomas-lee-29a033b2/" target="_blank" rel="noreferrer noopener">My LinkedIn</a>
-                    </li>
-                    <li className='social-icons'>
-                        <a href="https://docs.google.com/document/d/1U2hZsREZIIE2-TiM1Z9EYZIfEq5h5BeK/edit?usp=sharing&ouid=105605796702403291415&rtpof=true&sd=true" target="_blank" rel="noreferrer noopener">My Resume</a>
-                    </li>
-                </ul>
+                <li className='social-icons'>
+                    <a href="mailto: t.k.hobbes43@gmail.com">
+                    <FiMail/>
+                    </a>
+                </li>
+                <li className='social-icons'>
+                    <a href="https://github.com/tkhobbes43" 
+                    target="_blank" 
+                    rel="noreferrer noopener">
+                    <FiGithub/>
+                    </a>
+                </li>
+                <li className='social-icons'>
+                    <a href="https://www.linkedin.com/in/thomas-lee-29a033b2/" target="_blank"
+                    rel="noreferrer noopener">
+                    <FiLinkedin/>
+                    </a>
+                </li>
+                <li className='social-icons'>
+                    <a href="https://docs.google.com/document/d/1U2hZsREZIIE2-TiM1Z9EYZIfEq5h5BeK/edit?usp=sharing&ouid=105605796702403291415&rtpof=true&sd=true" 
+                    target="_blank" 
+                    rel="noreferrer noopener">
+                    <FiFile/>
+                    </a>
+                </li>
             </div>
         </div>
     )
