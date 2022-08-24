76f01c5c304825c11f02a0afe69f1ee58026c3bd
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-23 23:40:00 -0500

    it works, header, about, and footer renders

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import myPic from '../../assets/images/myPic';
+import myPic from '../../assets/images/mypic.jpg';
 
 function About() {
     return (
