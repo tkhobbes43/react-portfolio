c3c28afd13b0a095b1622c002a7d6b5ab9c0ca26
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-23 23:35:50 -0500

    going to add a profile pic for the about page to see if it renders

---
+++ src/components/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import myPic from '../../assets/images/myPic';
+
+function About() {
+    return (
+        <Container className='title'>
+            <Row>
+                <Col>
+                <center><img src={myPic} className="my-pic" alt="Tommy-Profile"/></center>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default About;
