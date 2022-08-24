728c2f9e9f0c6fd1ec563aaa5199bc65017cbd89
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-24 00:10:05 -0500

    got rid of stuff in contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,44 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function Contact() {
-    // Create form object with useState
-    const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
-    const [message, setMessage] = useState('');
-    const [success, setSuccess] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-
-    // To ensure email sent ribbon does not appear on load
-    useEffect(() => {
-        setSuccess(false);
-    }, [])
-
-    const handleInputChange = (e) => {
-        // Getting the value and name of the input which triggered the change
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
-
-        // Based on the input type, we set the state of either email, name, and message
-
-        if (inputType === 'email') {
-            setEmail(inputValue);
-        } else if (inputType === 'name') {
-            setName(inputValue);
-        } else {
-            setMessage(inputValue);
-        }
-    };
-
     return (
         <div>
             <h2>Contact Me</h2>
             <div>
                 <form>
                     <label>Name</label><br />
-                    <input placeholder="Name" /><br />
+                    <input placeholder="Your Name" /><br />
                     <label>Email</label><br />
-                    <input placeholder="your.email@here.com" /><br />
+                    <input placeholder="Your.email@here.com" /><br />
                     <label>Message</label><br />
                     <textarea placeholder="Your message here" /><br />
                     <button>
