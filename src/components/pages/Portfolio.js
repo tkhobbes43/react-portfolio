e8cbe9b23605d7fca021aa3f94aa7f5be30a09bb
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-26 20:27:07 -0500

    had to add 2 more projects to portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,26 +5,38 @@ const projects = [
     {   
         name: "Tiny Triumphs",
         link: "https://tinytriumphs.herokuapp.com/", 
-        image: "/assets/tinytriumphs.png",
+        image: "./assets/images/tinytriumphs.png",
         tech: ["Node.js", "Express.js", "Bcrypt", "MySQL", "Sequelize", "jQuery", "Handlebars.js", "Moment.js", "Multer"]
     }, 
     {   
         name: "Productivity Tiles",
         link: "https://anacecyflores1.github.io/ProductivityTiles/",
-        image: "https://github.com/anacecyflores1/ProductivityTiles/blob/main/screenshot.jpg",
+        image: "./assets/images/ProductivityTiles.jpg",
         tech: ["JavaScript", "HTML", "CSS", "jQuery", "jQuery UI", "Google Fonts API", "Images API", "Quotes Rapid API"]
     }, 
     {   
         name: "Weather Dashboard",
         link: "https://tkhobbes43.github.io/weather-dashboard/",
-        image: "https://github.com/tkhobbes43/weather-dashboard/blob/main/assets/images/Capture.PNG",
+        image: "./assets/images/weather-dashboard.PNG",
         tech: ["HTML", "CSS", "JavaScript", "Moment.js", "Open Weather API", "Google Fonts API", "Bootstrap", "jQuery"]
     }, 
     { 
         name: "My Work Day Scheduler",
         link: "https://tkhobbes43.github.io/my-work-day-scheduler/",
-        image: "https://github.com/tkhobbes43/my-work-day-scheduler/blob/main/assets/images/Capture.PNG",
+        image: "./assets/images/my-work-day-scheduler.PNG",
         tech: ["HTML", "CSS", "JavaScript", "Moment.js", "Font Awesome API", "Bootstrap", "Google Fonts API", "jQuery"]
+    },
+    { 
+        name: "Text Editor",
+        link: "https://rocky-brushlands-15740.herokuapp.com/",
+        image: "./assets/images/text-editor.png",
+        tech: ["indexedDB", "Progressive Web App", "Service Worker", "Webpack", "Workbox", "JavaScript", "Node.js", "Express.js", "Concurrently"]
+    },
+    { 
+        name: "My Social Network API",
+        link: "https://github.com/tkhobbes43/my-social-network-api",
+        image: "./assets/images/Capture.PNG",
+        tech: ["MongoDB", "Mongoose", "JavaScript", "Node.js", "Express.js", "API Routes"]
     }
 ];
 
