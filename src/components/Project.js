6abae2ca53e439e7f3ee70ec278e7258a72b6c4a
Tommy Lee <t.k.hobbes43@gmail.com>
2022-08-28 21:28:07 -0500

    changed projects to show as cards, but still need to refine and style cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,23 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Project({ project }) {
     return (
-        <div className="max-w-sm md:min-w-[20rem] rounded overflow-hidden shadow-lg mb-3 border border-gray-300 md:mx-3">
-            <img className="w-fit" src={project.image} alt="Display" />
-            <div className="px-6 py-4">
-                <div className="font-bold text-slate-200 text-xl mb-2">
-                    {project.name}
-                </div>
-                <a href={project.link} className="text-slate-300 text-base hover:text-slate-600 cursor-pointer">
-                    {project.link}
-                </a>
-            </div>
-            <div className="px-6 py-4">
-                {/* Make an array of techs used in the project.tech, and loop through them to create a given bubble */}
-                {project.tech.map((tech, index) => {
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={project.image} alt="Display" />
+            <Card.Body>
+                <Card.Title>{project.name}</Card.Title>
+                <Card.Text>
+                    Technologies Used: {project.tech.map((tech, index) => {
                     return <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 my-2">{tech}</span>
-                })}
-            </div>
-        </div>
-    )
+                })} 
+                </Card.Text>
+                <Button variant="link">{project.link}</Button>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default Project;
