c9785d6d734d4d200f64350af9189045106fc666
Tommy Lee <t.k.hobbes43@gmail.com>
2022-09-01 18:03:07 -0500

    added google fonts for h1, h2, h3, project, and project tech

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,10 +7,10 @@ function Project({ project }) {
         <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={project.image} alt="Display" />
             <Card.Body>
-                <Card.Title>{project.name}</Card.Title>
+                <Card.Title className="project">{project.name}</Card.Title>
                 <Card.Text>
                     Technologies Used: {project.tech.map((tech, index) => {
-                    return <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 my-2">{tech}</span>
+                    return <span className="project-tech"key={index}>{tech}</span>
                 })} 
                 </Card.Text>
                 <Button variant="link">{project.link}</Button>
