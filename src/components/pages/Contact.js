554411204f3a9a6fc4b717baad85a54106dadc64
Tommy Lee <t.k.hobbes43@gmail.com>
2022-09-01 18:54:14 -0500

    making fields for contact page bigger

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,11 +7,11 @@ function Contact() {
             <div>
                 <form>
                     <label>Name</label><br />
-                    <input placeholder="Your Name" /><br />
+                    <input placeholder="Your Name" size="50" /><br />
                     <label>Email</label><br />
-                    <input placeholder="Your.email@here.com" /><br />
+                    <input placeholder="Your.email@here.com" size="50" /><br />
                     <label>Message</label><br />
-                    <textarea placeholder="Your message here" /><br />
+                    <textarea placeholder="Your message here" rows="10" cols="100"/><br />
                     <button>
                         Submit
                     </button>
