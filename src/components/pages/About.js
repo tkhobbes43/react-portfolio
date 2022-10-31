5971d9de256dec9b6f9b420423f7f49e4314e97e
Tommy Lee <t.k.hobbes43@gmail.com>
2022-10-31 15:06:13 -0500

    another bio update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,7 +14,7 @@ function About() {
                     </p>
                     <p>I am a graduate of Northwestern University's Coding Bootcamp and earned a certificate in full stack web development.  With my newly developed skills, I hope to start fresh and gain as much insight as I possibly can.
                     </p>
-                    <p>Known as an adaptable and motivated problem solver passionate about scalable production services and design to implement user-facing products.  I applied aspects of agile development in 2 of my main projects. I worked on a team of four for Productivity Tiles and a team of three for Tiny Triumphs.  I am excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
+                    <p>Known as an adaptable and motivated problem solver passionate about scalable production services and design to implement user-facing products.  I applied aspects of agile development in all 3 of my main projects. I worked on a team of four for Productivity Tiles, a team of three for Tiny Triumphs, and a team of four for MernCave HQ.  I am excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
                     </p>
                 </Col>
                 <Col>
